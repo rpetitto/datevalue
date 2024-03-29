f385b3a914476f7c409c6164f7a536539706f241
rpetitto <r.petitto@gmail.com>
2024-03-29 14:34:04 -0400

    Update function.js

--- function.js
+++ function.js
@@ -3,6 +3,5 @@ window.function = function (dateInput, dateFormat) {
   const dt = DateTime.fromJSDate(dateInput.value);
   const formatString = dateFormat.value ?? "";
   const formattedDate = dt.toFormat(formatString);
-  return "j";
   return parseInt(formattedDate, 10);
 }
