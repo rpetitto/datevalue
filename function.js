8b436f08c9a6c65a6cfa09caee8ad3415a80ea90
rpetitto <r.petitto@gmail.com>
2024-03-29 15:39:59 -0400

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,6 @@
 var DateTime = luxon.DateTime;
 window.function = function (dateInput, dateFormat) {
-  const dt = DateTime.fromJSDate(dateInput.value);
+  const dt = DateTime.fromFormat(dateInput.value);
   const formatString = dateFormat.value ?? "";
   const formattedDate = dt.toFormat(formatString);
   return parseInt(formattedDate, 10);
