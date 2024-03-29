a3269d84ef29945dfa203805ee8f946f53a88c59
rpetitto <r.petitto@gmail.com>
2024-03-28 23:56:26 -0400

    Update function.js

--- function.js
+++ function.js
@@ -1,17 +1,8 @@
-window.function = function (str, start, end) {
-  // For each parameter, its `.value` contains
-  // either its value in the type you've declared,
-  // or it's `undefined`.  This is a good place to
-  // extract the `.value`s and assign default
-  // values.
+window.function = function (dateColumn, valueType) {
+
   str = str.value ?? "";
   start = start.value ?? 0;
   end = end.value;
 
-  // Your function should return the exact type
-  // you've declared for the `result` in
-  // `glide.json`, or `undefined` if there's an
-  // error or no result can be produced, because a
-  // required input is `undefined`, for example.
   return str.substring(start, end);
 }
